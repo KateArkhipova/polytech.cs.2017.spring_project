89b5f3ac587ca8b86a5d5ce63fbbd5218bbd0a8b
Sonya2312 <ira23121998@mail.ru>
2017-05-20 19:06:13 +0400

    Добавление комментариев

--- src/gonki.c
+++ src/gonki.c
@@ -1,32 +1,63 @@
 #include "includes.h"
 #include "functions.h"
+/**
+\mainpage Gonki
+# Gonki_project
+Гонки- аркадная двухмерная игра. Она имеет вид сверху.
+
+## Getting Started
+Эта игра написана на #С с использованием библиотеки Allegro.
+
+### Directory structure
+The source code is organized as follows:
+
+Subdirectory | Description
+-------------|-------------------
+src/         | source files
+src/tests    | unit tests
+doc/         | documentation
+res/     | static resources
+
+*/
 /*!
 @file gonki.c
-@author Irina
-\brief This is main class which contains main function where all gamaplay happens
+@brief Главная функция проекта
+*  Главная функция проекта.В данной функции подключается библиотека Allegro5 и различные ее пакеты.Функция отвечает за работу дисплея .
+*@author Klimentova Irina - ira23121998@mail.ru \n
+    *@author Arkhipova Ekaterina - arkat98@mail.ru
+ \fn int main()
+##Игровой цикл можно разделить на 3 части:
+###Первая часть:
+** *Эта часть представляет собой набор команд для отрисовки меню*
+###Вторая часть:
+** *Здесь находится основной игровой цикл*
+###Третья часть:
+** *Цикл для отрисовки машин, уровня и всех объектов*
+ *
 */
-int main(){
-	int const gorizontal_disp = 800;//!< ширина экрана
-	int const vertical_disp = 600;//!< высота экрана
-	int user_status = 2;//!< если переменная 0-выйти из игры, если 1-войти в игру, если 2-меню
-	int FPS = 60;//!< частота обновления кадров
-	int left_border = 50;//!< отрисовка линий
-	int rigth_border = 450;//!< отрисовка линий
-	int cener_line = 250;//!< отрисовка линий
-	int change_dir_to_right = 0;//!<  двигать котика вправо
-	int change_dir_to_left = 0;//!<  двигать котика влево
-	int coordinates_player_x = 90;//!< максимально левое положение игрока
-	int coordinates_player_y = 400;//!< максимальное правое положение игрока
-	int enemies[3][3] = {{0,0,0},{0,0,0},{0,0,0}};//!< массив врагов; первый элемент - наличие врага, второй - его координаты, третий - дорога
-	int rest_time = 0;//!< время задержки перед выходом врагов
-	int enemies_spawn = 0;//!< переменная, которая говорит нужно ли выпускаь врагов
-	int timer_of_enemy_go = 0;//!< время между выходом врагов
-	int index_enemy = 0;//!< номер врага, которого нужно выпускать
-	int score = 0;//!< очки
-	int stage = 1;//!< уровень
-	float speed = 0;//!< скорость игры
-   
 
+int main()
+{
+	int const gorizontal_disp = 800; // ширина экрана
+	int const vertical_disp = 600;//высота экрана
+	int user_status = 2;// если переменная 0-выйти из игры, если 1-войти в игру, если 2-меню
+	int FPS = 60;// частота обновления кадров
+	int left_border = 50;//  отрисовка линий
+	int rigth_border = 450;//  отрисовка линий
+	int cener_line = 250;//отрисовка линий
+	int change_dir_to_right = 0;// двигать котика вправо
+	int change_dir_to_left = 0;//  двигать котика влево
+	int coordinates_player_x = 90;// максимально левое положение игрока
+	int coordinates_player_y = 400;// максимальное правое положение игрока
+	int enemies[3][3] = {{0,0,0},{0,0,0},{0,0,0}};// массив врагов; первый элемент - наличие врага, второй - его координаты, третий - дорога
+	int rest_time = 0;// время задержки перед выходом врагов
+	int enemies_spawn = 0;// переменная, которая говорит нужно ли выпускаь врагов
+	int timer_of_enemy_go = 0;// время между выходом врагов
+	int index_enemy = 0;// номер врага, которого нужно выпускать
+	int score = 0;// очки
+	int stage = 1;// уровень
+	float speed = 0;// скорость игры
+ 
 
    if(!al_init()) {
       fprintf(stderr, "failed to initialize allegro!\n");
@@ -83,7 +114,7 @@ int main(){
 			
 
 			menu:
-	   		//Отрисовываем меню, пока пользователь не нажмет кнопку
+	   		/*  Отрисовываем меню, пока пользователь не нажмет кнопку */
 	   		while(user_status == 2){
 	   			ALLEGRO_EVENT ev_menu;
 	   			al_wait_for_event(event_queue, &ev_menu);
@@ -106,11 +137,11 @@ int main(){
 			 	}
 	   		}
 
-	   		//основной игровой цикл
+	   		/* основной игровой цикл */
 	   		while(user_status == 1){
 	   			ALLEGRO_EVENT ev;
 	   			al_wait_for_event(event_queue, &ev);
-	   			al_draw_line(left_border , 0, left_border , vertical_disp, al_map_rgb(255,255,255),4); //отрисовываем границы движения
+	   			al_draw_line(left_border , 0, left_border , vertical_disp, al_map_rgb(255,255,255),4); /* отрисовываем границы движения */
 	   			al_draw_line(rigth_border, 0, rigth_border, vertical_disp, al_map_rgb(255,255,255),4);
 	   			al_draw_line(cener_line-4, 0, cener_line-4, vertical_disp, al_map_rgb(255,255,255),1);
 	   			al_draw_line(cener_line+4, 0, cener_line+4, vertical_disp, al_map_rgb(255,255,255),1);
@@ -124,7 +155,7 @@ int main(){
 	   			else{
 	   				al_draw_bitmap(unicorn, 350 , 180 , ALLEGRO_FLIP_HORIZONTAL);
 	   			}
-	   			//Распознование нажтых клавиш
+	   			/* Распознование нажтых клавиш */
 	   			
 	   			if(ev.type == ALLEGRO_EVENT_KEY_DOWN)
 			 	{
@@ -145,11 +176,11 @@ int main(){
 			 	}
 
 
-			 	//увеличение скорости с уровнями
+			 	/* увеличение скорости с уровнями */
 
 			 	speed = get_speed(stage);
 
-			 	//плавное изменение положения машинки игрока
+			 	/* плавное изменение положения машинки игрока */
 
 			 	if(change_dir_to_left){
 			 		coordinates_player_x -= 10*speed;
@@ -171,7 +202,7 @@ int main(){
 			 	}
 
 
-			 	//Задержка перед началом игры
+			 	/* Задержка перед началом игры*/
 
 			 	rest_time = get_if(rest_time,enemies_spawn);
 			 	
@@ -181,12 +212,12 @@ int main(){
 			 	}
 
 
-			 	//генерация встречных машин
+			 	/* генерация встречных машин */
 
 			 	if(enemies_spawn){
 
 			 		
-			 		//время "выпускания" новой машины
+			 		/* время "выпускания" новой машины*/
 			 		if(timer_of_enemy_go > 120/speed){
 			 			enemies[index_enemy][0] = 1;
 			 			enemies[index_enemy][2] = rand() % 2 - 1 ; 
@@ -198,10 +229,10 @@ int main(){
 			 			timer_of_enemy_go++;
 			 		}
 
-			 		//цикл для отрисовки машин 
+			 		/* цикл для отрисовки машин */
 			 		for(int i = 0; i < 3; i++){
 			 			if(enemies[i][0]){
-			 				//цикл проверки на столкновение
+			 				/* цикл проверки на столкновение */
 			 				if(enemies[i][2]){
 			 					// al_draw_filled_rectangle( 290, enemies[i][1] - 170 , 410, enemies[i][1], al_map_rgb(200,170,150));
 			 					al_draw_bitmap(cat_enemy, 290 , enemies[i][1] - 170 , 0);	
@@ -260,16 +291,16 @@ int main(){
 			 			}
 			 		}
 			 	}
-			 	// отрисовка уровня и колличества наюранных очков
+			 	/* отрисовка уровня и колличества наюранных очков */
 			 	al_draw_textf(font, al_map_rgb(255,255,255), rigth_border + 150 , 30 ,ALLEGRO_ALIGN_CENTRE, "SCORE: %d" , score);
 	   			al_draw_textf(font, al_map_rgb(255,255,255), rigth_border + 150 , 60 ,ALLEGRO_ALIGN_CENTRE, "STAGE: %d" , stage);
 	   			al_draw_filled_rectangle( 510, 90 , 790, 130, al_map_rgb(17,28,39));
 	   			al_draw_textf(font, al_map_rgb(255,255,255), rigth_border + 200 , 100 ,ALLEGRO_ALIGN_CENTRE, "Press Esc to exit to menu");
 	   			
-	   			//увеличение уровня, если требуется
+	   			/* увеличение уровня, если требуется */
 				stage = get_stage(stage, score);
 
-	   			//отрисовывание всех объектов
+	   			/* отрисовывание всех объектов */
 			 	if(al_is_event_queue_empty(event_queue))
 		      		{
 		      			al_flip_display();
@@ -282,7 +313,7 @@ int main(){
     
 
   
-   	//Уничтожение созданных объектов библиотеки аллегро
+   	/* Уничтожение созданных объектов библиотеки аллегро */
     al_flip_display();
     al_destroy_display(display);
     al_destroy_timer(timer);
